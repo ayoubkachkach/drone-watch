ad1f0e566aa252cccc1417067933e887484a63e0
ayoubkachkach <ayoub.kachkach@gmail.com>
2019-03-22 15:57:26 +0100

    Add homepage and favicon fields to Website dataclass

--- scraping/website.py
+++ scraping/website.py
@@ -25,26 +25,31 @@ class Website():
         body_class CSS class of body.
     """
     name: str
+    homepage: str
     seed_urls: List[str]
     url_patterns: Match
     relative_url: bool
     title_class: str
     body_class: str
     date_class: str
+    favicon: str
     next_page: Callable[[Response], str]
 
 
 DAWN = Website(
     name='dawn',
+    homepage='https://www.dawn.com/',
     seed_urls=['https://www.dawn.com/archive/'],
     url_patterns=re.compile(r'https:\/\/www\.dawn\.com\/news\/[^#]*'),
     relative_url=False,
     title_class='story__title',
     body_class='story__content',
     date_class='story__time',
+    favicon='https://www.dawn.com/favicon.ico',
     next_page=pagination.next_page_dawn),
 REUTERS = Website(
     name='reuters',
+    homepage='https://www.reuters.com/',
     seed_urls=[
         'https://uk.reuters.com/news/archive/worldnews?view=page&page=1&pageSize=10'
     ],
@@ -54,9 +59,11 @@ REUTERS = Website(
     title_class='ArticleHeader_headline',
     body_class='StandardArticleBody_body',
     date_class='ArticleHeader_date',
+    favicon='https://www.reuters.com/resources/images/favicon.ico',
     next_page=pagination.next_page_reuters),
 PBS = Website(
     name='pbs',
+    homepage='https://www.pbs.org/'
     seed_urls=['https://www.pbs.org/newshour/world/page/1'],
     url_patterns=re.compile(
         r'https:\/\/www\.pbs\.org\/newshour\/world\/(?!page).*'),
@@ -64,6 +71,7 @@ PBS = Website(
     title_class='post__title',
     body_class='body-text',
     date_class='post__date',
+    favicon='https://www.pbs.org/favicon.ico',
     next_page=pagination.next_page_pbs)
 
 websites = {'DAWN': DAWN, 'REUTERS': REUTERS, 'PBS': PBS}
